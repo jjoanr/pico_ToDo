4a6771ea973231deeaa09864cd475898a9e1512e
jjoanr <jjoanrodrig@gmail.com>
2024-06-03 14:10:18 +0200

    multi line task descriptions + cross tasks done

--- include/tasks.h
+++ include/tasks.h
@@ -5,8 +5,17 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
-#define MAX_LENGTH 15
+#define MAX_LENGTH 42 // CHARS_PER_ROW * 2 rows
 #define MAX_TASKS 10
+/*
+ * 5 bit wide characters
+ * 1 bit space between characters
+ * 5*8 + 22 spaces = 127bits + 1bit at the end (end has 2 bit separation)
+ */
+#define CHARS_PER_ROW 21
+#define Y_START_ROW_1 24
+#define Y_START_ROW_2 34
+#define TASK_TEXT_MAX_ROWS 2
 
 /*
  * @brief Enum for return values.
@@ -45,5 +54,6 @@ void display_time(uint hour, uint minute);
 int prior_task(void);
 int mark_task_done(void);
 void draw_interface(void);
+void draw_cross_task(void);
 
 #endif /* TASKS_H */
